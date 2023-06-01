7a2d33f8a82ba4d2b87b45df41f009ef12127479
AndreeaBrandusa <andreea.brandusa01@e-uvt.ro>
2023-06-01 10:40:14 +0300

    Fix https error

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,13 +1,21 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace rina.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        public async Task<bool> SaveAsync()
+        {
+            var changesNumber = await SaveChangesAsync();
+
+            return changesNumber != 0;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -31,7 +31,6 @@ else
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
